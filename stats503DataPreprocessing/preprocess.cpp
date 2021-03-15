89e0a8ead8bbb7f113ff8c67ddcd6c1166d66f2c
Brody E <67604397+brodyee@users.noreply.github.com>
2021-03-15 13:15:36 -0400

    Completed Main() outline
    line 18 need updating. The rest should be good, just check if need all the variables given. May have to add a tiny bit more.

--- stats503DataPreprocessing/preprocess.cpp
+++ stats503DataPreprocessing/preprocess.cpp
@@ -6,24 +6,39 @@
 // map as a .csv
 
 #include <fstream>
-#include <map>
+#inlcude <string>
+#include <stringstream>
+#include <map> //probably unordered_map
 #include "CONST.h"
 #include "helperFunctions.h"
 using namespace std;
 
 int main(int argc,
          char *argv[]) {
-  map<string, double[NUM_OF_OBSERVATIONS]> data;
+  map<string, container> data;
+  vector<string> row;
   fstream inFile;
-  string filePath, comma, varTime, varName, line;
+  string filePath, line;
   int age, gender, admitType;
   double currVarVal;
 
   for (int i = 1; i <= NUM_OF_OBSERVATIONS; i++) {
     filePath = mainFilePath + to_string(i) + ".csv";
+    inFile.open(filePath, ios::in);
+           
     while (inFile >> line) {
-      // stuff
+      row.clear();
+             
+      getline(inFile, line);
+      stringstream s(line);
+      
+      row = readRow(s);
+             
+      addToData(data, row);
     }
+           
+    addDataToFiles(data, i);
+    data.clear();
   }
 
   return 0;
